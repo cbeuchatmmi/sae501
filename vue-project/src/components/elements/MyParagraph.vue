6ed2da158811de2cf175e32149ea65a22753a4d3
cbeuchatmmi <95022983+cbeuchatmmi@users.noreply.github.com>
2024-01-09 13:19:25 +0100

    feat(paragraph) : add MyParagraph

---
+++ vue-project/src/components/elements/MyParagraph.vue
@@ -0,0 +1,36 @@
+<script setup>
+defineProps({
+    p: String,
+    content: String,
+})
+
+const p1 = 'p1';
+const p2 = 'p2';
+
+</script>
+
+<template>
+    <div class="paragraph">
+        <p v-if="p === p1" class="paragraph__p1">{{ content }}</p>
+        <p v-if="p === p2" class="paragraph__p2">{{ content }}</p>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.paragraph {
+
+
+    &__p1 {
+        font-size: $mirror ;
+    }
+
+    &__p2 {
+        font-size: $body;
+    }
+
+
+}
+</style>
+
+
+
