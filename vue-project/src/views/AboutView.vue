df36108639b27e32b7961a3782b9b5a0a578733b
cbeuchatmmi <95022983+cbeuchatmmi@users.noreply.github.com>
2024-01-09 09:43:58 +0100

    initialisation du projet

---
+++ vue-project/src/views/AboutView.vue
@@ -0,0 +1,25 @@
+<script setup>
+import DefaultLayout from '@/components/layouts/DefaultLayout.vue'
+</script>
+<template>
+  <DefaultLayout>
+    <template #header> Nouveau Header </template>
+    Page about
+    <template #aside>
+      <nav>
+        <ul>
+          <li><a href="#">Aside link 1</a></li>
+          <li><a href="#">Aside link 2</a></li>
+          <li><a href="#">Aside link 3</a></li>
+        </ul>
+      </nav>
+    </template>
+
+
+
+
+    <template #footer> Nouveau Footer </template>
+  </DefaultLayout>
+</template>
+
+<style></style>
---
+++ vue-project/src/views/HomeView.vue
@@ -0,0 +1,24 @@
+<script setup>
+import DefaultLayout from '@/components/layouts/DefaultLayout.vue'
+
+</script>
+
+<template>
+  <DefaultLayout>
+    <template #header>
+      <nav>
+        <ul>
+          <li><a href="#">Nav link 1</a></li>
+          <li><a href="#">Nav link 2</a></li>
+          <li><a href="#">Nav link 3</a></li>
+        </ul>
+      </nav>
+    </template>
+
+
+
+    <template #footer>
+    </template>
+  </DefaultLayout>
+</template>
+<style lang="scss"></style>
