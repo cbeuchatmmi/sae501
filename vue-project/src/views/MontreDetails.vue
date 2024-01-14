f8d0700a9a14ec51e8a27e25b73e3ed0c4f3fda7
cbeuchatmmi <95022983+cbeuchatmmi@users.noreply.github.com>
2024-01-14 14:08:37 +0100

    update montre

--- vue-project/src/views/MontreDetails.vue
+++ vue-project/src/views/MontreDetails.vue
@@ -1,18 +1,3 @@
-<template>
-    <DefaultLayout>
-        <h1>Détails de la Montre</h1>
-        <div v-if="montre">
-            {{ montre.form_montre }} - {{ montre.boitier_fond }} - {{ montre.bracelet_texture }} - {{ montre.montre_prix }}€
-            <ThreeJs :fond="montre.boitier_fond" :boitier="montre.form_montre" :bracelet="montre.bracelet_texture" />
-            <button @click="deleteMontre">Supprimer la Montre</button>
-        </div>
-        <div v-else>
-            Chargement des détails de la montre...
-        </div>
-        <template #footer> Nouveau Footer </template>
-    </DefaultLayout>
-</template>
-  
 <script setup>
 import { ref, onMounted } from 'vue';
 import { useRoute, useRouter } from 'vue-router';
@@ -29,12 +14,27 @@ const client = axios.create({
 
 const montre = ref([]);
 const userId = localStorage.getItem('userId');
+const updatedMontre = ref({
+    form_montre: '',
+    boitier_fond: '',
+    bracelet_texture: '',
+    panier: false,
+});
+const boitiers = ref([]);
+const bracelets = ref([]);
 
 const getMontreDetails = async () => {
     try {
         const response = await client.get(`/montres/id/${montreId.value}`);
         montre.value = response.data;
-        console.log("montre", montre.value)
+        console.log("montre", montre.value);
+        // Définir les valeurs initiales de updatedMontre
+        updatedMontre.value = {
+            form_montre: montre.value.form_montre,
+            boitier_fond: montre.value.boitier_fond,
+            bracelet_texture: montre.value.bracelet_texture,
+            panier: false,
+        };
     } catch (error) {
         console.error('Erreur lors de la récupération des détails de la montre :', error.message);
     }
@@ -53,8 +53,79 @@ const deleteMontre = async () => {
     }
 };
 
+const updateMontre = async () => {
+    try {
+        await client.put(`/montres/${montreId.value}/update`, updatedMontre.value);
+        // Mettez à jour les données après la modification si nécessaire
+        montre.value = await getMontreDetails();
+        console.log("montre.value", montre.value)
+    } catch (error) {
+        console.error('Erreur lors de la mise à jour de la montre :', error.message);
+    }
+};
+
+const getBraceletsAndBoitiers = async () => {
+    try {
+        const [braceletsResponse, boitiersResponse] = await Promise.all([
+            client.get('/bracelets'),
+            client.get('/boitiers'),
+        ]);
+
+        bracelets.value = braceletsResponse.data;
+        console.log("braceletsResponse", bracelets.value)
+        boitiers.value = boitiersResponse.data;
+        console.log("boitiersResponse", boitiers.value)
+    } catch (error) {
+        console.error('Erreur lors de la récupération des bracelets et boitiers :', error.message);
+    }
+};
+
 onMounted(() => {
     getMontreDetails();
+    getBraceletsAndBoitiers();
 });
 </script>
-  
+
+<template>
+    <DefaultLayout>
+        <h1>Détails de la Montre</h1>
+        {{ montre.form_montre }} - {{ montre.boitier_fond }} - {{ montre.bracelet_texture }} - {{ montre.montre_prix }}€
+        <ThreeJs :fond="updatedMontre.boitier_fond" :boitier="updatedMontre.form_montre"
+            :bracelet="updatedMontre.bracelet_texture" />
+        <button @click="deleteMontre">Supprimer la Montre</button>
+
+
+        <div>
+            <h1>Modifier la Montre</h1>
+            <form @submit.prevent="updateMontre">
+                <label for="form_montre">Forme de la Montre:</label>
+                <select v-model="updatedMontre.form_montre" required>
+                    <option value="carre">Carre</option>
+                    <option value="rond">Rond</option>
+                </select>
+
+                <label for="boitier_fond">Boîtier de Fond:</label>
+                <select v-model="updatedMontre.boitier_fond" required>
+                    <!-- Options dynamiques à partir des données récupérées précédemment -->
+                    <option v-for="boitier in boitiers" :value="boitier.boitier_fond" :key="boitier.id_boitier">
+                        {{ boitier.boitier_fond }}
+                    </option>
+                </select>
+
+                <label for="bracelet_texture">Texture du Bracelet:</label>
+                <select v-model="updatedMontre.bracelet_texture" required>
+                    <!-- Options dynamiques à partir des données récupérées précédemment -->
+                    <option v-for="bracelet in bracelets" :value="bracelet.bracelet_texture" :key="bracelet.id_bracelet">
+                        {{ bracelet.bracelet_texture }}
+                    </option>
+                </select>
+
+                <label for="panier">Ajouter au Panier:</label>
+                <input type="checkbox" v-model="updatedMontre.panier" />
+
+                <button type="submit">Modifier</button>
+            </form>
+        </div>
+        <template #footer> Nouveau Footer </template>
+    </DefaultLayout>
+</template>
