5cfbc23878f40074f1cfbd533329e11cbe2718ee
cbeuchatmmi <95022983+cbeuchatmmi@users.noreply.github.com>
2024-01-14 21:54:00 +0100

    css de la page panier

--- vue-project/src/components/elements/MyPanier.vue
+++ vue-project/src/components/elements/MyPanier.vue
@@ -1,6 +1,7 @@
 <script setup>
-import { ref, onMounted } from 'vue';
+import { ref, onMounted, computed } from 'vue';
 import axios from 'axios';
+import Threejs from './threejs.vue';
 
 const montres = ref([]);
 
@@ -30,6 +31,11 @@ const updatePanier = async (idMontre) => {
     }
 };
 
+// Propriété calculée pour obtenir le total des prix des montres
+const totalPrix = computed(() => {
+    return montres.value.reduce((total, montre) => total + montre.montre_prix, 0);
+});
+
 // Appeler la fonction lors de la montée du composant
 onMounted(() => {
     getMontresPanier();
@@ -37,16 +43,51 @@ onMounted(() => {
 </script>
 
 <template>
-    <div>
-        <h1>Montres dans le panier :</h1>
-        <ul>
-            <li v-for="montre in montres" :key="montre.id_montre">
-                {{ montre.montre_prix }}€
-                <!-- Ajoutez le bouton pour mettre à jour le panier -->
-                <button @click="updatePanier(montre.id_montre)">Suppr</button>
-            </li>
-        </ul>
+    <div class="panier">
+        <h1 class="panier__title">Montres dans le panier :</h1>
+
+        <div class="panier__card" v-for="montre in montres" :key="montre.id_montre">
+            <Threejs :fond="montre.boitier_fond" :boitier="montre.form_montre" :bracelet="montre.bracelet_texture" />
+            <div>
+                <p class="panier__card--text">{{ montre.montre_prix }}€</p>
+                <button class="panier__card--button" @click="updatePanier(montre.id_montre)">Suppr</button>
+            </div>
+        </div>
+
+        <!-- Afficher le total des prix -->
+        <p class="panier__total">Total: {{ totalPrix }}€</p>
     </div>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+.panier {
+    &__title {
+        font-size: $h2;
+        color: $secondary-color;
+    }
+
+    &__card {
+        display: flex;
+
+        &--text {
+            font-size: $h2;
+            color: $primary-color;
+        }
+
+        &--button {
+            background: none;
+            border: none;
+            border-radius: rem(16);
+            background-color: $primary-color;
+            padding: rem(20);
+            font-size: $button;
+            cursor: pointer;
+        }
+    }
+
+    &__total {
+        font-size: $h2;
+        color: $black;
+    }
+}
+</style>
--- vue-project/src/views/PanierView.vue
+++ vue-project/src/views/PanierView.vue
@@ -1,33 +1,10 @@
 <script setup>
-import MyConnexion from '../components/elements/MyConnexion.vue';
-import MyIcon from '../components/elements/MyIcon.vue';
-
+import MyPanier from '../components/elements/MyPanier.vue';
 </script>
 <template>
-  <template>
-    <nav>
-      <div class="header">
-        <div class="header__left">
-          <a href="/">
-            <MyIcon name="" />
-          </a>
-          <a href="/add" class="header__left--link">Création</a>
-        </div>
-        <div class="header__right">
-          <a href="/panier">
-            <MyIcon name="panier" />
-          </a>
-          <a href="/login">
-            <MyIcon name="profil" />
-          </a>
-        </div>
-      </div>
-    </nav>
-  </template>
-
-  <MyConnexion />
-
-  <template> Nouveau Footer </template>
+  <div>
+    <MyPanier />
+  </div>
 </template>
 
 <style></style>
