67129e760c2a426591ece15cb050da7999ea3382
cbeuchatmmi <95022983+cbeuchatmmi@users.noreply.github.com>
2024-01-14 21:03:29 +0100

    css de la page AddMontre

--- vue-project/src/views/AddView.vue
+++ vue-project/src/views/AddView.vue
@@ -1,48 +1,12 @@
 <script setup>
 import AddMontre from '../components/elements/AddMontre.vue';
-import MyIcon from '../components/elements/MyIcon.vue';
-
 
 </script>
 
 
 
 <template>
-    <template>
-        <nav>
-            <div class="header">
-                <div class="header__left">
-                    <a href="/">
-                        <MyIcon name="" />
-                    </a>
-                    <a href="/add" class="header__left--link">Création</a>
-                </div>
-                <div class="header__right">
-                    <a href="/panier">
-                        <MyIcon name="panier" />
-                    </a>
-                    <a href="/login">
-                        <MyIcon name="profil" />
-                    </a>
-                </div>
-            </div>
-        </nav>
-    </template>
-
-
     <AddMontre />
-
-
-    <template>
-        <p>salut</p>
-    </template>
 </template>
-<style lang="scss">
-/* Ajoutez du style si nécessaire */
-
-.three {
-    width: rem(20);
-    height: rem(10);
-}
-</style>
+<style lang="scss"></style>
   
