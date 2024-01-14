9419d6a7eaeffd16f86c3b919d26bb315078c12b
cbeuchatmmi <95022983+cbeuchatmmi@users.noreply.github.com>
2024-01-14 18:22:28 +0100

    height div liste

--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -1,15 +1,16 @@
 <script setup>
 import ListeMontre from '../components/elements/ListeMontre.vue';
-import MyPanier from '../components/elements/MyPanier.vue';
 </script>
 
 <template>
-  <div>
-    <MyPanier />
-  </div>
-
-  <div>
+  <div class="liste">
     <ListeMontre />
   </div>
 </template>
+
+<style lang="scss">
+.liste {
+  height: 100vh;
+}
+</style>
 
