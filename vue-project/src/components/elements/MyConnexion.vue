3395eb5b07b14ec7975b693e7fcf169c3d1c24ac
cbeuchatmmi <95022983+cbeuchatmmi@users.noreply.github.com>
2024-01-14 18:41:48 +0100

    css MyConnexion

--- vue-project/src/components/elements/MyConnexion.vue
+++ vue-project/src/components/elements/MyConnexion.vue
@@ -55,22 +55,81 @@ const logout = () => {
 </script>
 
 <template>
-    <div>
-        <h2>Connexion</h2>
-        <form @submit.prevent="login">
-            <label for="user_email">Email:</label>
-            <input v-model="loginData.user_email" type="email" required>
+    <div class="connexion">
+        <h2 class="connexion__title">Connexion</h2>
+        <form class="connexion__form" @submit.prevent="login">
+            <label class="connexion__form--label" for="user_email">Email:</label>
+            <input class="connexion__form--input" v-model="loginData.user_email" type="email" required>
 
-            <label for="user_password">Mot de passe:</label>
-            <input v-model="loginData.user_password" type="password" required>
+            <label class="connexion__form--label" for="user_password">Mot de passe:</label>
+            <input class="connexion__form--input" v-model="loginData.user_password" type="password" required>
 
-            <!-- Affichage du statut de connexion -->
-            <p v-if="userId">Utilisateur connecté! <button @click="logout">Se déconnecter</button></p>
-            <p v-else>Veuillez vous connecter.
-                <button type="submit">Se connecter</button>
+            <p class="connexion__form--content" v-if="userId">Utilisateur connecté!
+                <button class="connexion__form--button" @click="logout">Se déconnecter</button>
+            </p>
+            <p class="connexion__form--content" v-else>Veuillez vous connecter.
+                <button class="connexion__form--button" type="submit">Se connecter</button>
             </p>
         </form>
     </div>
 </template>
 
-<style></style>
+<style lang="scss">
+.connexion {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-items: center;
+    background-color: $light-black;
+
+
+    &__title {
+        font-size: $h2;
+        color: $secondary-color;
+    }
+
+    &__form {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-items: center;
+        color: $secondary-color;
+
+
+        &--label {
+            margin-top: rem(16);
+        }
+
+        &--input {
+            padding: rem(16);
+            width: rem(256);
+            border: none;
+
+            border-radius: rem(16);
+        }
+    }
+
+    &__form {
+
+
+        &--content {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-items: center;
+            margin-top: rem(16);
+            margin-bottom: rem(16);
+        }
+
+        &--button {
+            background: none;
+            border: none;
+            border-radius: rem(16);
+            background-color: $primary-color;
+            padding: rem(20);
+            font-size: $button;
+            cursor: pointer;
+        }
+    }
+}
+</style>
